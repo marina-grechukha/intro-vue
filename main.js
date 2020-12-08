729868ad73325961da3d81cc7834b01c873e3c6b
marina.grechukha <marina.grechukha@gmail.com>
2020-12-08 22:10:47 +0200

    3. Conditional Rendering

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = new Vue({
     el: '#app',
     data: {
         product: 'Cat',
-        image: 'https://images.unsplash.com/photo-1604713965157-bba82828d7f9?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTF8MzMzMDQyODd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-        link: 'https://unsplash.com/collections/33304287/the-cats-'
+        image: './assets/red-cat.jpg',
+        link: 'https://unsplash.com/collections/33304287/the-cats-',
+        inStock: true,
+        onSale: true
     }
 })
