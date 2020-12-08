8f7e8f968e9be30f1b1e79bb05f3d8614554e7f2
marina.grechukha <marina.grechukha@gmail.com>
2020-12-08 20:24:15 +0200

    2. Attribute Binding

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = new Vue({
     el: '#app',
     data: {
-        product: 'Cats',
-        description: 'A pair of warm fuzzy cats'
+        product: 'Cat',
+        image: 'https://images.unsplash.com/photo-1604713965157-bba82828d7f9?ixid=MXwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTF8MzMzMDQyODd8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+        link: 'https://unsplash.com/collections/33304287/the-cats-'
     }
 })
